092d8a9a97a898bb9b704839f703055e2d4171b4
Aleksey B <boev.aleksey@gmail.com>
2015-12-17 22:13:10 +1300

    Implemented msg_announce_userid, msg_user_answered, make_question and updated message handler

--- lib/protocol.rb
+++ lib/protocol.rb
@@ -33,6 +33,22 @@ class Protocol
     end
   end
 
+  def self.msg_announce_userid(id_from, msg_type, msg_body)
+    userid = msg_body
+    user = User.find_by_id(msg_body)
+    user.socket_id = id_from
+    user.save
+  end
+
+  def self.msg_user_answered(id_from, msg_type, msg_body)
+    game = Game.find_by_socket_id(id_from).first
+    game_details = JSON.parse(game.details)
+    game_details[Constants::JSON_GAME_PLAYERS][id_from] = Game::PLAYER_STATUS_ANSWERED
+    game.details = game_details.to_json
+    game.save
+    #players = game_details[Constants::JSON_GAME_PLAYERS].keys.except(id_from)
+  end
+
   def self.parse_msg(msg_json)
     id_from = msg_json[Constants::JSON_SOCK_MSG_FROM]
     msg_type = msg_json[Constants::JSON_SOCK_MSG_TYPE]
@@ -50,7 +66,11 @@ class Protocol
     elsif (msg_type == Constants::SOCK_MSG_TYPE_SOCKET_CLOSE)
       self.msg_socket_close(id_from, msg_type, msg_body)
     elsif (msg_type == Constants::SOCK_MSG_TYPE_QUIT_GAME)
-      self.msg_socket_close(id_from, msg_type, id_from)    
+      self.msg_socket_close(id_from, msg_type, id_from)
+    elsif (msg_type == Constants::SOCK_MSG_TYPE_PLAYER_ANSWERED)
+      self.msg_user_answered(id_from, msg_type, msg_body)
+    elsif (msg_type == Constants::SOCK_MSG_TYPE_ANNOUNCE_USERID)
+      self.msg_announce_userid(id_from, msg_type, msg_body)
     end
   
     res = { :result => Constants::RESULT_OK }
@@ -60,6 +80,12 @@ class Protocol
   def self.make_msg(id_to, msg_type, msg_body)
     message = {Constants::JSON_SOCK_MSG_TO => id_to, Constants::JSON_SOCK_MSG_TYPE => msg_type, Constants::JSON_SOCK_MSG_BODY => msg_body}.to_json
     return message
+  end
+
+  def self.make_question(question, options, answer_id)
+    question = {Constants::JSON_QST_QUESTION => question,
+        Constants::JSON_QST_OPTIONS => options,
+        Constants::JSON_QST_ANSWER_ID => answer_id}
   end
 
 end
