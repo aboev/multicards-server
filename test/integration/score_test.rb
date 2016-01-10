35313abbb514be07d156496b5ec2736263ba3cac
Aleksey B <boev.aleksey@gmail.com>
2016-01-11 01:55:30 +1300

    Added score test

--- test/integration/score_test.rb
+++ test/integration/score_test.rb
@@ -36,6 +36,50 @@ class ScoreTest < ActionDispatch::IntegrationTest
     assert_equal 0, scores[@@socket2.session_id]
   end
 
+  test "Should increase score after correct answer" do
+    user_id1 = register(@profile1)
+    user_id2 = register(@profile2)
+    new_game(user_id1, @@socket1.session_id)
+    new_game(user_id2, @@socket2.session_id)
+    sleep(1)
+
+    answer_id = filter(@@sock1_msg_list, Constants::SOCK_MSG_TYPE_NEW_QUESTION).first["msg_body"][Constants::JSON_QST_ANSWER_ID]
+    @@sock2_msg_list = []
+    @@sock1_msg_list = []
+    player_answer(@@socket1, answer_id, [])
+    update_client_status(@@socket1, Game::PLAYER_STATUS_WAITING)
+    update_client_status(@@socket2, Game::PLAYER_STATUS_WAITING)
+
+    sleep(1)
+    scores = filter(@@sock1_msg_list, Constants::SOCK_MSG_TYPE_NEW_QUESTION).first["msg_extra"]
+    score1 = scores[@@socket1.session_id]
+    score2 = scores[@@socket2.session_id]
+    assert_equal score1, 1
+    assert_equal score2, 0
+  end
+
+  test "Should not increase score after wrong answer" do
+    user_id1 = register(@profile1)
+    user_id2 = register(@profile2)
+    new_game(user_id1, @@socket1.session_id)
+    new_game(user_id2, @@socket2.session_id)
+    sleep(1)
+
+    answer_id = filter(@@sock1_msg_list, Constants::SOCK_MSG_TYPE_NEW_QUESTION).first["msg_body"][Constants::JSON_QST_ANSWER_ID]
+    @@sock2_msg_list = []
+    @@sock1_msg_list = []
+    player_answer(@@socket1, answer_id + 1, [])
+    update_client_status(@@socket1, Game::PLAYER_STATUS_WAITING)
+    update_client_status(@@socket2, Game::PLAYER_STATUS_WAITING)
+
+    sleep(1)
+    scores = filter(@@sock1_msg_list, Constants::SOCK_MSG_TYPE_NEW_QUESTION).first["msg_extra"]
+    score1 = scores[@@socket1.session_id]
+    score2 = scores[@@socket2.session_id]
+    assert_equal score1, 0
+    assert_equal score2, 0
+  end
+
   @@socket1.on :event do |msg|
     msg_json = JSON.parse(msg)
     @@sock1_msg_list << msg_json
