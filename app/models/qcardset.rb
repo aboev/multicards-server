cca8c9f6912d66524cc9cb4d117608bcba2fb921
Aleksey B <boev.aleksey@gmail.com>
2016-01-12 09:01:27 +1300

    Updated like_count

--- app/models/qcardset.rb
+++ app/models/qcardset.rb
@@ -2,10 +2,12 @@ class Qcardset < ActiveRecord::Base
 
   def add_like(userid)
     update_attributes likes: likes + [ userid ]
+    self.like_count = self.like_count + 1
   end
 
   def remove_like(userid)
     update_attributes likes: likes - [ userid ]
+    self.like_count = self.like_count - 1
   end
 
 end
