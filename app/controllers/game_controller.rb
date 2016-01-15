bb40a894aee9d9ecb2221a125424daafa33ca47d
Aleksey B <boev.aleksey@gmail.com>
2016-01-16 09:05:46 +1300

    Refactor new game

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -7,36 +7,62 @@ skip_before_filter :verify_authenticity_token
 def new
   gid = request.headers[Constants::HEADER_SETID]
   opponent_name = request.headers[Constants::HEADER_OPPONENTNAME]
-  setid = -1
-  game = nil
+
+  if gid == nil
+    ret_error
+  elsif Utils.get_qcardset(gid) == nil
+    ret_error
+  end
+
+  setid = Utils.parse_gid(gid)[1]
   if (opponent_name == nil)
-    game = Game.where(status: Game::STATUS_SEARCHING_PLAYERS, setid: setid).first
-  elsif (opponent_name != "-1")
+    game_public = Game.where(status: Game::STATUS_SEARCHING_PLAYERS, setid: setid).first
+    if game_public != nil
+      join_and_start(game_public, @user)
+      ret_ok(JSON.parse(game_public.details))
+    else
+      game_public = init_and_join(setid, true, @user)
+      ret_ok(JSON.parse(game_public.details))
+    end
+  elsif (opponent_name == "-1")
+    game_private = init_and_join(setid, false, @user)
+    ret_ok(JSON.parse(game_private.details))
+  else
     opponent = User.find_by_name(opponent_name)
     if opponent != nil
-      game = Game.find_by_socket_id(opponent.socket_id, Game::STATUS_WAITING_OPPONENT).first
+      game_private = Game.find_by_socket_id(opponent.socket_id, Game::STATUS_WAITING_OPPONENT).first
+      join_and_start(game_private, @user)
+      ret_ok(JSON.parse(game_private.details))
+    else
+      ret_error
     end
   end
-  if gid != nil
-    cardset = Utils.get_cardset(gid)
-    if cardset != nil
-      game = Game.where(status: Game::STATUS_SEARCHING_PLAYERS, setid: cardset.id).first
-      setid = cardset.id
-    end
-  end
-  if (game != nil)
-    game.join_player(@user)
-    game.start_game
-  else
-    game = Game.new
-    game.init(setid, (opponent_name == "-1") ? false : true)
-    game.join_player(@user)
-  end
-  game_details = JSON.parse(game.details)
-  msg = { :result => Constants::RESULT_OK, :data => game_details }
+end
+
+def ret_error
+  msg = { :result => Constants::RESULT_ERROR }
   respond_to do |format|
     format.json  { render :json => msg }
   end
+end
+
+def ret_ok (data)
+  msg = { :result => Constants::RESULT_OK, :data => data }
+  respond_to do |format|
+    format.json  { render :json => msg }
+  end
+end
+
+def join_and_start(game, user)
+  game.join_player(user)
+  game.start_game
+end
+
+def init_and_join(setid, rnd_opp, user)
+  game = Game.new
+  game.init(setid, rnd_opp)
+  game.join_player(user)
+  return game
 end
 
 end
