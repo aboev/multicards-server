968bc7f81e0161036b6898adf5e0a43c97ef2e7a
Aleksey B <boev.aleksey@gmail.com>
2016-01-24 07:15:51 +1300

    Implemented support for inverted cardsets

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,5 @@
 require 'protocol'
+require 'constants'
 
 class Question
 
@@ -10,25 +11,37 @@ class Question
   QSTATUS_RIGHT_ANSWER = 2
 
   def self.make_random(qtype, set_id, question_id)
+    cardset = Qcardset.where(:cardset_id => set_id).first
+    invert = false
+    invert = true if ((cardset != nil) and (cardset.tags.include?(Constants::DB_FLAG_INVERT)))
+
     if qtype == QTYPE_MULTI_CHOICE
-      return make_multichoice_qquestion(set_id, question_id)
+      return make_multichoice_qquestion(set_id, question_id, invert)
     elsif qtype == QTYPE_DIRECT_INPUT
-      question = make_directinput_qquestion(set_id, question_id)
+      question = make_directinput_qquestion(set_id, question_id, invert)
       return question if question != nil
       return make_random(QTYPE_MULTI_CHOICE)
     end
   end
 
-  def self.make_multichoice_qquestion(set_id, question_id)
+  def self.make_multichoice_qquestion(set_id, question_id, invert)
     return if set_id == -1
-    cards = Qcard.where(:cardset_id => set_id).order("RANDOM()").first(4)
-    answer_id = rand(4)
-    options = cards.map {|option| option.definition}
-    question = Protocol.make_question(cards[answer_id].term, options, answer_id, question_id)
+    question = nil
+    if (!invert)
+      cards = Qcard.where(:cardset_id => set_id).order("RANDOM()").first(4)
+      answer_id = rand(4)
+      options = cards.map {|option| option.definition}
+      question = Protocol.make_question(cards[answer_id].term, options, answer_id, question_id)
+    else
+      cards = Qcard.where(:cardset_id => set_id).order("RANDOM()").first(4)
+      answer_id = rand(4)
+      options = cards.map {|option| option.term}
+      question = Protocol.make_question(cards[answer_id].definition, options, answer_id, question_id)
+    end
     return question
   end
 
-  def self.make_directinput_qquestion(set_id, question_id)
+  def self.make_directinput_qquestion(set_id, question_id, invert)
     return if set_id == -1
     cards = Qcard.where(:cardset_id => set_id).order("RANDOM()").first(20)
     card = nil
