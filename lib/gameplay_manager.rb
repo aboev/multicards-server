9fd1e4d2459fc764b819358c06c6af5713958285
Aleksey B <boev.aleksey@gmail.com>
2016-02-05 05:20:13 +1300

    Implemented gameplay_manager and added DISCOVERER_BONUS

---
+++ lib/gameplay_manager.rb
@@ -0,0 +1,19 @@
+require 'question'
+require 'constants'
+require 'game'
+
+class GameplayManager
+
+  def self.start_game(game)
+    game_details = JSON.parse(game.gameplay_data)
+    cardset = Qcardset.where(:cardset_id => game.setid).first
+    if ((cardset != nil) and (!cardset.flags.include?(Constants::FLAG_DISCOVERED)))
+      cardset.add_flag(Constants::FLAG_DISCOVERED)
+      cardset.save
+      if ((game.player1_socketid != nil) and (game.player1_socketid.length > 0))
+        game.give_bonus(game.player1_socketid, Constants::BONUS_DISCOVERER)
+      end
+    end
+  end
+
+end
