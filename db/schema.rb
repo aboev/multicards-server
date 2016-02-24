ea16cd684571e3957ba5c786f1ea474c988fba04
Aleksey B <boev.aleksey@gmail.com>
2016-02-25 03:43:25 +1300

    Added name to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160213080709) do
+ActiveRecord::Schema.define(version: 20160224144246) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -134,6 +134,7 @@ ActiveRecord::Schema.define(version: 20160213080709) do
     t.integer  "score",      default: 0
     t.text     "pushid"
     t.integer  "status"
+    t.string   "name"
   end
 
 end
