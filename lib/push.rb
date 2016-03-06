7774c36b5034252b10f9cd2bbf74f16cc618a66d
Aleksey B <boev.aleksey@gmail.com>
2016-03-06 19:21:41 +0300

    Added logging

--- lib/push.rb
+++ lib/push.rb
@@ -20,9 +20,9 @@ class PushSender
     request.body = data.to_json
     response = http.request(request)
     if response.kind_of? Net::HTTPSuccess
-      #log.debug("Notification sent to user " + user.id.to_s + ", response message: " + response.body)
+      logger.debug "Notification sent to user " + user.id.to_s + ", response message: " + response.body
     else
-      #log.debug("Failed to send notification to user " + user.id.to_s + ", response message: " + response.body)
+      logger.debug "Failed to send notification to user " + user.id.to_s + ", response message: " + response.body
     end
   end
 
